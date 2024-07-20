56562b172edcdffcbe7697c643b97f24b1a5875a
kanji5413 <kanji5413@gmail.com>
2024-07-20 16:09:24 +0530

    anji

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
-# terraform-modules/eks/variables.tf
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+  default     = "default"
 }

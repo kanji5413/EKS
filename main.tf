cdb2c293d2278f6b3d2563f39d24d8413f92cf95
kanji5413 <kanji5413@gmail.com>
2024-07-20 20:49:01 +0530

    added

--- main.tf
+++ main.tf
@@ -1,17 +1,40 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.31.0"
     }
   }
 }
 
-
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = {
-    Name = "main-vpc"
-  }
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = merge(
+    local.common_tags,
+    {
+      Name = var.vpc_name
+    }
+  )
+}
+
+resource "aws_subnet" "public-subnets" {
+  count                   = length(var.public_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.vpc_name}-public-subnet-${count.index + 1}"
+    }
+  )
 }
--- variables.tf
+++ variables.tf
@@ -14,3 +14,28 @@ variable "profile" {
   type        = string
   default     = "default"
 }
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "public_cidrs" {
+  description = "A list of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "A list of availability zones"
+  type        = list(string)
+}
+
+variable "business_division" {
+  description = "The business division"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment"
+  type        = string
+}

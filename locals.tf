16304f8b6420cd89564aa2e31235e818047fb5e8
kanji5413 <kanji5413@gmail.com>
2024-07-27 18:59:12 +0530

    anji

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
+#no need to use this here you can use locls.tf in your local machine somewhere ypur using modules thta place it self only 
+
 locals {
   owners      = var.business_division
   environment = var.environment
